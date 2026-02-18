61bcb0a95a052d882ccd3a9778b7385618d4d5cc
Damico707 <damian707rangel@gmail.com>
2026-02-18 08:49:04 -0500

    Santis version

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 
-let myurl = ('https://rickandmortyapi.com/api/character')
-let valor = prompt ("Categoria a buscar")
+let input = prompt ("Categoria a buscar")
+switch (input) {
+    case "1":
+        valor = "character"
+        valor+="?name="+prompt("nombre")
+    case "2":
+        valor = "location"
+    case "3":
+        valor
+}
+let myurl = ('https://rickandmortyapi.com/api/'+valor)
 
 
 fetch(myurl)
 .then(response=> response.json())
 .then(data=> {
-    console.log(data ['results'][0][valor]);
+    console.table(data['results']);
 });
